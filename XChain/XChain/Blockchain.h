d8043b3c82e9fddd5e4aa7170ebe967f3c138c5e
Filip Volkanoski <f.volkanoski@gmail.com>
2019-11-23 22:11:07 +0100

    Created a VS solution for the project, added VS gitignore

--- XChain/XChain/Blockchain.h
+++ XChain/XChain/Blockchain.h
@@ -13,15 +13,15 @@ using namespace std;
 
 class Blockchain {
 public:
-    Blockchain();
+	Blockchain();
 
-    void AddBlock(Block bNew);
+	void AddBlock(Block bNew);
 
 private:
-    uint32_t _nDifficulty;
-    vector<Block> _vChain;
+	uint32_t _nDifficulty;
+	vector<Block> _vChain;
 
-    Block _GetLastBlock() const;
+	Block _GetLastBlock() const;
 };
 
 #endif //TESTCHAIN_BLOCKCHAIN_H
